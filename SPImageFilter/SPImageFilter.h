380001c924ed3b6c23c67788d2357204da2cefa4
heo <wone2287@gmail.com>
2020-09-28 17:34:44 +0900

    Initialize project

---
+++ SPImageFilter/SPImageFilter.h
@@ -0,0 +1,18 @@
+//
+//  SPImageFilter.h
+//  SPImageFilter
+//
+//  Created by won heo on 2020/09/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPImageFilter.
+FOUNDATION_EXPORT double SPImageFilterVersionNumber;
+
+//! Project version string for SPImageFilter.
+FOUNDATION_EXPORT const unsigned char SPImageFilterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPImageFilter/PublicHeader.h>
+
+
