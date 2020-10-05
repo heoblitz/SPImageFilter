4aa357c42d88b7b9d0fc9f8c473b2765c8f8ac81
heo <wone2287@gmail.com>
2020-10-05 23:17:20 +0900

    Change folder structure

---
+++ Sources/SPImageFilter/SPImageContext.swift
@@ -0,0 +1,12 @@
+//
+//  SPImageContext.swift
+//  SPImageFilter
+//
+//  Created by won heo on 2020/10/05.
+//
+
+import UIKit
+
+public struct SPImageContext {
+    static let ciContext: CIContext = CIContext(options: nil)
+}
